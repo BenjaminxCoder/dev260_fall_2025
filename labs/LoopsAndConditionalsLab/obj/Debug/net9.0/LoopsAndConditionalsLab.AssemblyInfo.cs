15ed1cc7b88af21c6dcba4261b41d80b418b7ed5
BenjaminxCoder <bengixcoder@outlook.com>
2025-10-29 09:29:55 -0700

    Clean-up nodes

--- labs/LoopsAndConditionalsLab/obj/Debug/net9.0/LoopsAndConditionalsLab.AssemblyInfo.cs
+++ labs/LoopsAndConditionalsLab/obj/Debug/net9.0/LoopsAndConditionalsLab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoopsAndConditionalsLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7246e8196425fcea25f233fc734dcdf3d83bd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d30d1a8f1dbf0ae6e40e6e359880e2e9d84ee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoopsAndConditionalsLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoopsAndConditionalsLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
